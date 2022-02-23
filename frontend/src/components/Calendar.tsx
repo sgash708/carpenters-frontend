6040e1430f5220ff129cf7b40e07bbbaeee3b08e
sgash708 <sugash7081023@gmail.com>
2022-02-23 16:05:33 +0900

    feat: カレンダー機能作成

---
+++ frontend/src/components/Calendar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import TimeGrid from '@fullcalendar/timegrid';
+import DayGrid from '@fullcalendar/daygrid';
+import Interaction from '@fullcalendar/interaction';
+
+const Calendar: React.FC = props => {
+  return (
+    <div>
+      <FullCalendar
+        locale="ja"
+        plugins={[TimeGrid, DayGrid, Interaction]}
+        initialView="timeGridWeek"
+        slotDuration="00:30:00"
+        selectable={true}
+        businessHours={{
+          daysOfWeek: [1, 2, 3, 4, 5],
+          startTime: '00:00',
+          endtime: '24:00'
+        }}
+        weekends={true}
+        titleFormat={{
+          year: 'numeric',
+          month: 'short'
+        }}
+        headerToolbar={{
+          start: 'title',
+          center: 'prev, next, today',
+          end: 'dayGridMonth,timeGridWeek'
+        }}
+      />
+    </div>
+  )
+}
+
+export default Calendar
